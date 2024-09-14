88fa8c1bba02c56b7ec4a4cdafa55c9cd38d5852
kleryxx <126974469+Vlad-Lipinskyi@users.noreply.github.com>
2024-09-14 14:36:23 +0300

    ssss

---
+++ index.js
@@ -0,0 +1,31 @@
+// 1 Завдання 
+
+const buttonEl = document.querySelector(".button");
+const textEl = document.querySelector(".text");
+
+buttonEl.textContent = textEl.textContent;
+
+
+// 2 Завдання
+
+const title = document.querySelector(".title");
+const imageEl = document.querySelector(".img");
+const checkboxEl = document.querySelector(".checkbox");
+
+imageEl.src = "./img/batman.png";
+
+
+// 3 Завдання
+
+const youtImgEl = document.querySelector(".yout-img");
+const linkEl = document.querySelector(".google-link");
+
+linkEl.href = "https://www.google.com/chrome/";
+youtImgEl.alt = "chrome";
+
+
+// 4 Завдання
+
+const listEL = document.querySelector(".list");
+const firstEL = listEL.firstElementChild;
+firstEL.textContent = "One one 1 1"
